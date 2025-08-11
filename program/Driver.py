6226d2539c4ebcd19a16660ea88cdfa899ec209b
Gustavo Cruz <cru22779@uvg.edu.gt>
2025-08-11 16:19:49 -0600

    init Lab2

--- program/Driver.py
+++ program/Driver.py
@@ -1,14 +1,22 @@
 import sys
 from antlr4 import *
-from MiniLangLexer import MiniLangLexer
-from MiniLangParser import MiniLangParser
+from SimpleLangLexer import SimpleLangLexer
+from SimpleLangParser import SimpleLangParser
+from type_check_visitor import TypeCheckVisitor
 
 def main(argv):
     input_stream = FileStream(argv[1])
-    lexer = MiniLangLexer(input_stream)
+    lexer = SimpleLangLexer(input_stream)
     stream = CommonTokenStream(lexer)
-    parser = MiniLangParser(stream)
-    tree = parser.prog()  # We are using 'prog' since this is the starting rule based on our MiniLang grammar, yay!
+    parser = SimpleLangParser(stream)
+    tree = parser.prog()
+
+    visitor = TypeCheckVisitor()
+    try:
+        visitor.visit(tree)
+        print("Type checking passed")
+    except TypeError as e:
+        print(f"Type checking error: {e}")
 
 if __name__ == '__main__':
     main(sys.argv)
